dcb2eb2b2f9177f75dce98542248b6d371354e60
tarunchopra1012 <tarun.chopra10@gmail.com>
2024-06-18 14:37:36 +0530

    Add scopes and other route methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,6 @@ class User extends Authenticatable
 
     public function address()
     {
-        return $this->hasOne(Address::class);
+        return $this->hasOne(Address::class)->latest(); // use the latest scope
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Address;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +17,48 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $user = User::find(1);
+
+    // $address = $user->address()->latest()->first(); // It will give us the laravel collection of the models 
+    // dd($address);
+
     return view('address', [
         'user' => $user
     ]);
+});
+
+Route::get('/create', function () {
+    $user = User::find(1);
+
+    $user->address()->create([
+        'line_1' => '40 Park West',
+        'line_2' => 'Romeo Wood',
+        'city' => 'Jallandhar',
+        'state' => 'Punjab',
+        'zip_code' => 54321
+    ]);
+
+    /*
+    Address::forceCreate([
+        'user_id' => 1,
+        'line_1' => '38 Park West',
+        'line_2' => 'East Wood',
+        'city' => 'Jallandhar',
+        'state' => 'Punjab',
+        'zip_code' => 54321
+    ]);
+    */
+});
+
+Route::get('/update', function () {
+    $user = User::find(1);
+
+    $user->address()->update([
+        'line_1' => '1 Eloquent Lane'
+    ]);
+});
+
+Route::get('/delete', function () {
+    $user = User::find(1);
+
+    $user->address()->delete();
 });
